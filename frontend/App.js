3a4e9f85fcaa0b482e007ad7528c35ded9c24d5e
megankulshekar <91085343+megankulshekar@users.noreply.github.com>
2024-01-20 14:49:18 -0800

    added multi page compatibility

--- frontend/App.js
+++ frontend/App.js
@@ -1,12 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import Login from './pages/login';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    // <View style={styles.container}>
+    //   <Text>Open up App.js to start working on your app!</Text>
+    //   <StatusBar style="auto" />
+    // </View>
+  <NavigationContainer>
+    <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={Login} />
+    </Stack.Navigator>
+  </NavigationContainer>
   );
 }
 
