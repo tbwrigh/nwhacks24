114d712c8cc8d260e97dc64d227fa74189c016ee
Tyler Wright <tyler.wright5146@gmail.com>
2024-01-20 19:15:57 -0800

    vaults

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,7 @@
-from fastapi import FastAPI, Request, Body, HTTPException, status, Depends
+from fastapi import FastAPI, Request, Body, HTTPException, status, Depends, File, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
+from fastapi.responses import StreamingResponse
 
 from dotenv import load_dotenv
 from os import getenv
@@ -10,7 +11,10 @@ from pymemcache.client import base
 import random
 
 from models.user import User
+from models.vault import Vault
 from db import DB
+
+from minio import Minio
 
 
 load_dotenv()
@@ -104,3 +108,58 @@ def protected_endpoint(user: dict = Depends(get_authenticated_user_from_session_
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not authenticated")
     return {"message": "This user can connect to a protected endpoint after successfully autheticated", "user": user}
 
+@app.post("/new-vault")
+def new_vault(user: User = Depends(get_authenticated_user_from_session_id), name=Body(...)):
+    if user is None:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not authenticated")
+    bucket_name = f"{user.username}-vault-{name['name']}-{random.randint(100000000, 999999999)}"
+    app.state.minio_client.make_bucket(bucket_name)
+    with app.state.db.session() as session:
+        new_vault = Vault(name=name['namefi'], user_id=user.id, bucket_name=bucket_name)
+        session.add(new_vault)
+        session.commit()
+    return {"message": "Vault created successfully"}
+
+@app.get("/vaults")
+def get_vaults(user: User = Depends(get_authenticated_user_from_session_id)):
+    if user is None:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not authenticated")
+    with app.state.db.session() as session:
+        vaults = session.query(Vault).filter(Vault.user_id == user.id).all()
+        return {"vaults": vaults}
+
+@app.get("/vault/objects/{vault_name}")
+def get_vault(vault_name: str, user: User = Depends(get_authenticated_user_from_session_id)):
+    if user is None:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not authenticated")
+    with app.state.db.session() as session:
+        vault = session.query(Vault).filter(Vault.user_id == user.id, Vault.name == vault_name).first()
+        if vault is None:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Vault not found")
+        objects = app.state.minio_client.list_objects(vault.name)
+        return {"objects": objects}
+
+@app.post("/vault/objects/{vault_name}")
+async def upload_object(vault_name: str, user: User = Depends(get_authenticated_user_from_session_id), file: UploadFile = File(...)):
+    if user is None:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not authenticated")
+    with app.state.db.session() as session:
+        vault = session.query(Vault).filter(Vault.user_id == user.id, Vault.name == vault_name).first()
+        if vault is None:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Vault not found")
+        file_content = await file.read()
+        file_size = len(file_content)
+        await file.seek(0)
+        app.state.minio_client.put_object(vault.name, file.filename, file.file, length=file_size, content_type=file.content_type)
+        return {"message": "File uploaded successfully"}
+
+@app.get("/vault/objects/{vault_name}/{object_name}")
+def download_object(vault_name: str, object_name: str, user: User = Depends(get_authenticated_user_from_session_id)):
+    if user is None:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not authenticated")
+    with app.state.db.session() as session:
+        vault = session.query(Vault).filter(Vault.user_id == user.id, Vault.name == vault_name).first()
+        if vault is None:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Vault not found")
+        object = app.state.minio_client.get_object(vault.name, object_name)
+        return StreamingResponse(object, media_type="application/octet-stream")
